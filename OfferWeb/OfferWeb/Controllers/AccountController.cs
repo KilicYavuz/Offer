6fdae190042cc81ddb599f1b7308d967fe1986e5
Ahmet Erdoğan <ahmeteerdoan@gmail.com>
2020-06-23 00:48:18 +0300

    User Paneli menü vs.

---
+++ OfferWeb/OfferWeb/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OfferWeb.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hesabim()
+        {
+            return View();
+        }
+
+        public ActionResult Magazam()
+        {
+            return View();
+        }
+
+        public ActionResult Siparisler()
+        {
+            return View();
+        }
+
+        public ActionResult Favoriler()
+        {
+            return View();
+        }
+
+        public ActionResult Yorumlar()
+        {
+            return View();
+        }
+
+        public ActionResult UrunTalepleri()
+        {
+            return View();
+        }
+    }
+}
