4f722e615f29ec0dbd37df8fcfd6f45cf8426b44
Ahmet Erdoğan <ahmeteerdoan@gmail.com>
2020-06-22 23:17:51 +0300

    Menü

---
+++ OfferServer/Entities/Migrations/20200622192700_asd.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class asd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "SupplierProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "RequestProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "RequestOffers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "RequestOfferProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ProductTags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ProductOptions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "OrderProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "FavoriteLists");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "FavoriteListItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ErrorLogs");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Address");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Tags",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "SupplierProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Requests",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "RequestProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "RequestOffers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "RequestOfferProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ProductTags",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ProductOptions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "OrderProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Notifications",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "FavoriteLists",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "FavoriteListItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ErrorLogs",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Comments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "CartItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Brands",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Address",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
---
+++ OfferServer/Entities/Migrations/20200622192909_asd2.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class asd2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "SupplierProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "RequestProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "RequestOffers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "RequestOfferProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ProductTags");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ProductOptions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "OrderProducts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "FavoriteLists");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "FavoriteListItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ErrorLogs");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Address");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Tags",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "SupplierProducts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Requests",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "RequestProducts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "RequestOffers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "RequestOfferProducts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ProductTags",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ProductOptions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "OrderProducts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Notifications",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "FavoriteLists",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "FavoriteListItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ErrorLogs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Comments",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Categories",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "CartItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Brands",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Address",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "SupplierProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "RequestProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "RequestOffers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "RequestOfferProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ProductTags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ProductOptions");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "OrderProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "FavoriteLists");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "FavoriteListItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ErrorLogs");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Address");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Tags",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "SupplierProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Requests",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "RequestProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "RequestOffers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "RequestOfferProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "ProductTags",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "ProductOptions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "OrderProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Notifications",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "FavoriteLists",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "FavoriteListItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "ErrorLogs",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Comments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "CartItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Brands",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Address",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
